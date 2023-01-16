4bdb6ef547a41017dc14b9b02ff03b5a00d70d0c
HashanEranga <ccmrhe@gmail.com>
2023-01-16 17:40:48 +0530

    client - Created the broilerplate code segments

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './Assets/Styles/App.css';
-import {BrowseRouter, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Footer from './Layouts/Footer/Footer';
+import Nav from './Layouts/Nav/Nav'
+import Products from './Pages/Products/Products';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Nav/>
+      
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/products' element={<Products/>}/>
+      </Routes>
+
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ client/src/Pages/Products/Products.js
@@ -0,0 +1,9 @@
+function Products() {
+    return(
+        <>
+            <h1>Products Component</h1>
+        </>
+    )
+}
+
+export default Products;
